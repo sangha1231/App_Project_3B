d2c3b5e6a28d1304ea286735bfe1cbd339cd9597
qwg914 <bjh6627@gmail.com>
2025-05-07 11:01:49 +0900

    Delete project_eb_flutter_2 directory

--- project_eb_flutter_2/lib/pages/traffic_page.dart
+++
@@ -1,9 +0,0 @@
-// lib/pages/traffic_page.dart
-import 'package:flutter/material.dart';
-class TrafficPage extends StatelessWidget {
-  const TrafficPage({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Traffic Page')));
-  }
-}
